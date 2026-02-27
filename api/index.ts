a0a220ae419a28dc9ee37d814e716f3ad016bbf4
benaya josua <benaya.josua@gmail.com>
2026-02-27 13:21:51 +0700

    fix : perbaiki vercel.json1

--- api/index.ts
+++ api/index.ts
@@ -2,7 +2,7 @@ import type { VercelRequest, VercelResponse } from "@vercel/node";
 
 export default function handler(_req: VercelRequest, res: VercelResponse) {
   res.status(200).json({
-    message: "Portfolio API - Serverless",
+    message: "halobenaya API backend berhasil",
     version: "1.0.0",
     endpoints: {
       auth: "/api/auth/*",
