4d0bc43a37662737b5d36fed8aa3718db6c367f9
benaya josua <benaya.josua@gmail.com>
2026-02-27 13:34:48 +0700

    deploy

--- api/[...catchall].ts
+++ api/[...catchall].ts
@@ -4,5 +4,5 @@ import { cors } from "../lib/cors";
 export default function handler(req: VercelRequest, res: VercelResponse) {
   cors(req, res);
   if (req.method === "OPTIONS") return res.status(200).end();
-  res.status(404).json({ message: "Not found" });
+  return res.status(404).json({ message: "Not found" });
 }
